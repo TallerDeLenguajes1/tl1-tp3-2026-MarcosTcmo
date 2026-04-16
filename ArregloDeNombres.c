aa7f826cd1d212255d337cbbb0f9504fab244de3
Ramirez Marcos Elias <tecmorym0@gmail.com>
2026-04-16 12:55:58 -0300

    segundo commit

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+void mostrarVector(char *vectorReceptor[], int cantidad);
+
 int main(){
     char *nombres[5],nombre[50];
     int tamanioCadena = 0;
@@ -12,10 +14,26 @@ int main(){
         tamanioCadena = strlen(nombre);
         nombres[i] = (char *) malloc ((tamanioCadena + 1) * sizeof(char));
         strcpy(nombres[i],nombre);
-        printf("Nombre guardado %s\n",nombres[i]); 
+        //printf("Nombre guardado %s\n",nombres[i]); 
+        
     }
+    mostrarVector(nombres,5);
+    for (int i = 0; i < 5; i++)
+    {
+        free(nombres[i]);
+    }
+    
     
 
     getchar();
     return 0;
+}
+
+void mostrarVector(char *vectorReceptor[], int cantidad){
+    printf("Nombres de los vectores almacenados: \n");
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("Nombre %d: %s\n",i+1,vectorReceptor[i]);       
+    }
+    
 }
