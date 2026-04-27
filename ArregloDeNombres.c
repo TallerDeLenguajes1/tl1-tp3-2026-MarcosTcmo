39ba59a906cc0f15a54903fbc88d0ff1d19470b9
Ramirez Marcos Elias <tecmorym0@gmail.com>
2026-04-26 23:42:06 -0300

    agrego el BuscarNombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,9 +3,10 @@
 #include <string.h>
 
 void mostrarVector(char *vectorReceptor[], int cantidad);
+void BuscarNombre(char *vectorReceptor[], int cantidad, char *nombreABuscar);
 
 int main(){
-    char *nombres[5],nombre[50];
+    char *nombres[5],nombre[50],*nombreABuscar;
     int tamanioCadena = 0;
     for (int i = 0; i < 5; i++)
     {
@@ -18,6 +19,11 @@ int main(){
         
     }
     mostrarVector(nombres,5);
+    printf("Ingrese un nombre que quiera buscar: ");
+    scanf("%s", &nombre);
+    nombreABuscar = (char *) malloc ((strlen(nombre) + 1) * sizeof(char));
+    strcpy(nombreABuscar,nombre);
+    BuscarNombre(nombres,5,nombreABuscar);
     for (int i = 0; i < 5; i++)
     {
         free(nombres[i]);
@@ -34,6 +40,12 @@ void mostrarVector(char *vectorReceptor[], int cantidad){
     for (int i = 0; i < cantidad; i++)
     {
         printf("Nombre %d: %s\n",i+1,vectorReceptor[i]);       
-    }
+    }    
+}
+
+void BuscarNombre(char *vectorReceptor[], int cantidad, char *nombreABuscar){
+    char *resultado;
+    resultado = strstr(vectorReceptor,nombreABuscar);
+    printf("%s", *resultado);
     
 }
